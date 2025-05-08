6980633e0f346045ba6691ad0ddf399a95673f6f
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2025-05-08 10:10:56 +0530

    BFSA

---
+++ BFS/Message_Route.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef TOWRIST
+    #define debug(...) cout << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__);
+#else
+    #define debug(...);
+#endif
+
+template <typename T> std::ostream &operator<<(std::ostream &stream, const vector<T> &vec) {for (size_t i = 0; i < vec.size(); i++) { stream << vec[i]; if (i != vec.size() - 1) stream << ' '; }; return stream; } template <typename T> std::istream &operator>>(std::istream &stream, vector<T> &vec) {for (T &x : vec) stream >> x; return stream; } template <typename T, typename U> std::ostream &operator<<(std::ostream &stream, const pair<T, U> &pr) {stream << pr.first << ' ' << pr.second; return stream; } template <typename T, typename U> std::istream &operator>>(std::istream &stream, pair<T, U> &pr) {stream >> pr.first >> pr.second; return stream; } template <typename A, typename B> string to_string(pair<A, B> p); template <typename A, typename B, typename C> string to_string(tuple<A, B, C> p); template <typename A, typename B, typename C, typename D> string to_string(tuple<A, B, C, D> p); string to_string(const string &s) { return '"' + s + '"'; } string to_string(char c) {string s; s += c; return s; } string to_string(const char *s) { return to_string((string)s); } string to_string(bool b) { return (b ? "1" : "0"); } string to_string(vector<bool> v) {bool first = true; string res = "{"; for (int i = 0; i < static_cast<int>(v.size()); i++) {if (!first) {res += ", "; } first = false; res += to_string(v[i]); } res += "}"; return res; } template <size_t N> string to_string(bitset<N> v) {string res = ""; for (size_t i = 0; i < N; i++) {res += static_cast<char>('0' + v[i]); } return res; } template <typename A> string to_string(A v) {bool first = true; string res = "{"; for (const auto &x : v) {if (!first) {res += ", "; } first = false; res += to_string(x); } res += "}"; return res; } template <typename A, typename B> string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; } template <typename A, typename B, typename C> string to_string(tuple<A, B, C> p) { return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")"; } template <typename A, typename B, typename C, typename D> string to_string(tuple<A, B, C, D> p) { return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")"; } void debug_out() { cout << endl; } template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) {cout << " " << to_string(H); debug_out(T...); }
+
+#define int             long long
+#define all(x)          x.begin(), x.end()
+#define double          long double
+#define endl            '\n'
+#define ff              first
+#define ss              second
+#define v               vector
+#define pb              push_back
+#define bit(num, i)     (num & (1ll << i))
+
+using pii = pair<int, int>;
+const bool multipleTestCases = false;
+v<int> dx{1, -1, 0, 0, 1, 1, -1, -1};
+v<int> dy{0, 0, 1, -1, 1, -1, 1, -1};
+string dir = "DURL";
+
+void solve()
+{
+  int n, m; cin >> n >> m;
+  v<v<int>> edges(m, v<int>(2)); cin >> edges;
+  v<v<int>> adj(n, v<int>());
+
+  for(int i = 0; i < m; i++){
+    int u = edges[i][0]-1, v = edges[i][1]-1;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  int start = 0, end = n-1;
+
+  queue<int> q;
+
+  v<int> dist(n, 1e9);
+  v<int> parent(n, -1);
+
+  q.push(start);
+  dist[start] = 1;
+  while(!q.empty()){
+    int curr = q.front();
+    q.pop();
+    for(int i = 0; i < adj[curr].size(); i++){
+      int currNode = adj[curr][i];
+      if(dist[currNode] > 1 + dist[curr]){
+        q.push(currNode);
+        dist[currNode] = 1 + dist[curr];
+        parent[currNode] = curr;
+      }
+    }
+  }
+  if(parent[end] == -1){
+    cout << "IMPOSSIBLE" << endl;
+    return;
+  }
+  cout << dist[end] << endl;
+  v<int> ans{end};
+  while(parent[end] != -1){
+    ans.push_back(parent[end]);
+    end = parent[end];
+  }
+  for(int i = ans.size()-1; i > -1; i--){
+    cout << ans[i] + 1<< " ";
+  }
+}  
+
+signed main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t = 1;
+    multipleTestCases and cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
