fbc116db9e011d65e3614cbd379c3b21db06001a
KeshavMakkad <keshavmakkad10@gmail.com>
2025-05-24 16:04:24 +0530

    Investigation

---
+++ Shortest Path/C_Investigation.cpp
@@ -0,0 +1,101 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <utility>
+#include <string>
+#include <bitset>
+#include <algorithm>
+using namespace std;
+
+#ifdef TOWRIST
+    #define debug(...) cout << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__);
+#else
+    #define debug(...);
+#endif
+
+template <typename T> std::ostream &operator<<(std::ostream &stream, const vector<T> &vec) {for (size_t i = 0; i < vec.size(); i++) { stream << vec[i]; if (i != vec.size() - 1) stream << ' '; }; return stream; } template <typename T> std::istream &operator>>(std::istream &stream, vector<T> &vec) {for (T &x : vec) stream >> x; return stream; } template <typename T, typename U> std::ostream &operator<<(std::ostream &stream, const pair<T, U> &pr) {stream << pr.first << ' ' << pr.second; return stream; } template <typename T, typename U> std::istream &operator>>(std::istream &stream, pair<T, U> &pr) {stream >> pr.first >> pr.second; return stream; } template <typename A, typename B> string to_string(pair<A, B> p); template <typename A, typename B, typename C> string to_string(tuple<A, B, C> p); template <typename A, typename B, typename C, typename D> string to_string(tuple<A, B, C, D> p); string to_string(const string &s) { return '"' + s + '"'; } string to_string(char c) {string s; s += c; return s; } string to_string(const char *s) { return to_string((string)s); } string to_string(bool b) { return (b ? "1" : "0"); } string to_string(vector<bool> v) {bool first = true; string res = "{"; for (int i = 0; i < static_cast<int>(v.size()); i++) {if (!first) {res += ", "; } first = false; res += to_string(v[i]); } res += "}"; return res; } template <size_t N> string to_string(bitset<N> v) {string res = ""; for (size_t i = 0; i < N; i++) {res += static_cast<char>('0' + v[i]); } return res; } template <typename A> string to_string(A v) {bool first = true; string res = "{"; for (const auto &x : v) {if (!first) {res += ", "; } first = false; res += to_string(x); } res += "}"; return res; } template <typename A, typename B> string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; } template <typename A, typename B, typename C> string to_string(tuple<A, B, C> p) { return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")"; } template <typename A, typename B, typename C, typename D> string to_string(tuple<A, B, C, D> p) { return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")"; } void debug_out() { cout << endl; } template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) {cout << " " << to_string(H); debug_out(T...); }
+
+#define int             long long
+#define all(x)          x.begin(), x.end()
+#define double          long double
+#define endl            '\n'
+#define ff              first
+#define ss              second
+#define v               vector
+#define pb              push_back
+#define bit(num, i)     (num & (1ll << i))
+
+using pii = pair<int, int>;
+const bool multipleTestCases = false;
+v<int> dx{1, -1, 0, 0, 1, 1, -1, -1};
+v<int> dy{0, 0, 1, -1, 1, -1, 1, -1};
+string dir = "DURL";
+int INF = 1e18;
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+    v<v<pii>> adj(n);
+    for(int i = 0; i < m; i++){
+        int u, v, d; cin >> u >> v >> d;
+        u--; v--;
+        adj[u].pb({d, v});
+    }
+    priority_queue<pii, v<pii>, greater<>> pq;
+
+    pq.push({0,0});
+
+    v<int> dist(n, INF);
+    v<int> paths(n, 0);
+    v<int> maxNodes(n, 0);
+    v<int> minNodes(n, INF);
+
+    dist[0] = 0;
+    int MOD = 1e9 + 7;
+    paths[0] = 1;
+    minNodes[0] = 1;
+    maxNodes[0] = 1;
+
+    while(!pq.empty()){
+        auto [currD, curr] = pq.top();
+        pq.pop();
+
+        if(dist[curr] < currD) continue;
+        for(auto [nbDist, nb] : adj[curr]){
+            int temp = currD + nbDist;
+            if(dist[nb] == temp){
+                paths[nb]+= paths[curr];
+                paths[nb] %= MOD;
+                minNodes[nb] = min(minNodes[nb], minNodes[curr] +1);
+                maxNodes[nb] = max(maxNodes[nb], maxNodes[curr] +1);
+            }
+            else if(dist[nb] > temp){
+                dist[nb] = temp;
+                paths[nb] = paths[curr];
+                minNodes[nb] = minNodes[curr] +1;
+                maxNodes[nb] = maxNodes[curr] +1;
+                pq.push({dist[nb], nb});
+            }
+        }
+    }
+    debug(dist);
+    cout << dist[n-1] << " " << paths[n-1] << " " << minNodes[n-1] -1 << " " << maxNodes[n-1] -1 << endl;
+
+}
+
+signed main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t = 1;
+    multipleTestCases and cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+}
